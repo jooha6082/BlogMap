52014e019190df382bc297a0822811e756f517d7
Joey Yu <yujongu@gmail.com>
2020-04-12 15:35:02 +0900

    Get rid of api key hiding bs. Need to do it later.

--- app/src/main/java/com/juha/blogmap/MainActivity.java
+++ app/src/main/java/com/juha/blogmap/MainActivity.java
@@ -1,32 +1,25 @@
 package com.juha.blogmap;
 
-import android.Manifest;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.location.Location;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
-import com.skt.Tmap.TMapGpsManager;
-import com.skt.Tmap.TMapMarkerItem;
-import com.skt.Tmap.TMapPoint;
-import com.skt.Tmap.TMapView;
-
-import java.util.ArrayList;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.skt.Tmap.TMapView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TMapView tmapview;
 
+    String TMAP_API_KEY = "l7xx822771dc1c964fb7bc81be7f579a5e9c";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tmapview = new TMapView(this);
-        tmapview.setSKTMapApiKey(BuildConfig.TMAP_API_KEY);
+        tmapview.setSKTMapApiKey(TMAP_API_KEY);
         RelativeLayout relativeLayout = findViewById(R.id.relativelayout_tmap);
         relativeLayout.addView(tmapview);
 
